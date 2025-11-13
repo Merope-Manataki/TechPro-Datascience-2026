9d0f598a9c1699d701a853dfb3433cff49c4a61a
George Kantasis <kantasisg@gmail.com>
2025-11-13 20:51:56 +0200

    max function

---
+++ week01/max.py
@@ -0,0 +1,15 @@
+
+def max(numbers_list):
+   max_temp = numbers_list[0]
+   for number in nubers_list:
+      if number > max_temp:
+         max_temp = number
+   return max_temp
+
+x = [3,6,1,6,37,8,48,8,2,7,3,35]
+print(f"the max value is {max(x)}")
+
+
+# ftiakse 3 input
+# printare to max tous
+
