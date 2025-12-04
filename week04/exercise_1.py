668baa66f9c2ee3c2870697452e31f8e0cb94ee4
George Kantasis <kantasisg@gmail.com>
2025-12-04 18:14:16 +0200

    week 04 exercise

---
+++ week04/exercise_1.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+M=5
+N=10
+A = np.random.randint(0,10,(N,M))
+B = np.random.randint(5,50,(M,2))
+
+weights_and_revenue = A.dot(B)
+print(A)
+print()
+print(B)
+print()
+print(weights_and_revenue)
+
+is_less_than_500 = weights_and_revenue[:,1] < 500
+
+print( weithgs_and_revenue[is_less_than_500,1].argmax() )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
